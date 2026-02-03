051df64df472fe23b1daa1c49d3d908475160f92
noureldin <noureldin2662002@gmail.com>
2026-02-04 01:12:51 +0200

    initial

---
+++ src/test/java/com/example/vetconnect/VetConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vetconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
